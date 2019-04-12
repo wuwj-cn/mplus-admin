4ba7a30f9a20f5ee09b8b1cdeeb851e86896afe5
wuwj-cn <wenjie.0617@gmail.com>
2019-04-12 18:38:55 +0800

    commit

--- src/app/routes/sys/sys-routing.module.ts
+++ src/app/routes/sys/sys-routing.module.ts
@@ -3,16 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { SysModuleComponent } from './setting/module/module.component';
 import { SysMenuComponent } from './setting/menu/menu.component';
 import { SysUserComponent } from './org/user/user.component';
-import { SysPostComponent } from './org/post/post.component';
 import { SysOrgComponent } from './org/org/org.component';
+import { SysPostComponent } from './org/post/post.component';
 
 const routes: Routes = [
 
   { path: 'module', component: SysModuleComponent },
   { path: 'menu', component: SysMenuComponent },
   { path: 'user', component: SysUserComponent },
-  { path: 'post', component: SysPostComponent },
-  { path: 'org', component: SysOrgComponent }];
+  { path: 'org', component: SysOrgComponent },
+  { path: 'post', component: SysPostComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/routes/sys/sys.module.ts
+++ src/app/routes/sys/sys.module.ts
@@ -4,22 +4,24 @@ import { SysRoutingModule } from './sys-routing.module';
 import { SysModuleComponent } from './setting/module/module.component';
 import { SysMenuComponent } from './setting/menu/menu.component';
 import { SysUserComponent } from './org/user/user.component';
-import { SysPostComponent } from './org/post/post.component';
 import { SysSettingModuleEditComponent } from './setting/module/edit/edit.component';
 import { SysSettingMenuEditComponent } from './setting/menu/edit/edit.component';
 import { SysOrgComponent } from './org/org/org.component';
 import { SysOrgOrgEditComponent } from './org/org/edit/edit.component';
+import { SysPostComponent } from './org/post/post.component';
+import { SysOrgPostEditComponent } from './org/post/edit/edit.component';
 
 const COMPONENTS = [
   SysModuleComponent,
   SysMenuComponent,
   SysUserComponent,
-  SysPostComponent,
-  SysOrgComponent];
+  SysOrgComponent,
+  SysPostComponent];
 const COMPONENTS_NOROUNT = [
   SysSettingModuleEditComponent,
   SysSettingMenuEditComponent,
-  SysOrgOrgEditComponent];
+  SysOrgOrgEditComponent,
+  SysOrgPostEditComponent];
 
 @NgModule({
   imports: [
