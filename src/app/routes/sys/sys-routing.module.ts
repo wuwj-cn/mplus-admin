7b545a5e0c1b69515fa7846014f8af9d4e462b6b
wuwj-cn <wenjie.0617@gmail.com>
2019-04-15 11:42:15 +0800

    初始化用户管理模块

--- src/app/routes/sys/sys-routing.module.ts
+++ src/app/routes/sys/sys-routing.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SysModuleComponent } from './setting/module/module.component';
 import { SysMenuComponent } from './setting/menu/menu.component';
-import { SysUserComponent } from './org/user/user.component';
 import { SysOrgComponent } from './org/org/org.component';
 import { SysPostComponent } from './org/post/post.component';
 import { SysDictComponent } from './setting/dict/dict.component';
+import { SysUserComponent } from './org/user/user.component';
 
 const routes: Routes = [
 
   { path: 'module', component: SysModuleComponent },
   { path: 'menu', component: SysMenuComponent },
-  { path: 'user', component: SysUserComponent },
   { path: 'org', component: SysOrgComponent },
   { path: 'post', component: SysPostComponent },
-  { path: 'dict', component: SysDictComponent }];
+  { path: 'dict', component: SysDictComponent },
+  { path: 'user', component: SysUserComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
